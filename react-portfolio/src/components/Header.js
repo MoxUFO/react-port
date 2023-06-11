c1d14b4201487e9c14095f1c7fced2bd65f6ef03
MOX UFO <mox.ufo@gmail.com>
2023-06-11 13:55:44 -0400

    replace bootstrap with material ui and started on the header

--- react-portfolio/src/components/Header.js
+++ react-portfolio/src/components/Header.js
@@ -1,10 +1,30 @@
-import React from "react";
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
+import Box from '@mui/material/Box';
+import '../styles/Header.css'
 // import NavTabs from "./NavTabs";
 
 function Header() {
     return (
-      <header>
-
+      <header class='my-header'>
+        <h2>Nelson Foster Jr.</h2>
+        <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        '& > *': {
+          m: 1,
+        },
+      }}
+    >
+      <ButtonGroup variant="text" aria-label="text button group">
+        <Button>About</Button>
+        <Button>Edu</Button>
+        <Button>Project</Button>
+      </ButtonGroup>
+    </Box>
       </header>
       
     )
