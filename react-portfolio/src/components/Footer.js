9cb2094a2d374d5f535c9e5f3bb11981a00f12b4
MOX UFO <mox.ufo@gmail.com>
2023-06-15 11:48:27 -0400

    contact form can be invoked from the phone icon in footer

--- react-portfolio/src/components/Footer.js
+++ react-portfolio/src/components/Footer.js
@@ -1,21 +1,92 @@
 import * as React from 'react';
+import '../styles/Footer.css'
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import TwitterIcon from '@mui/icons-material/Twitter';
-import '../styles/Footer.css'
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Slide from '@mui/material/Slide';
+import AddIcCallIcon from '@mui/icons-material/AddIcCall';
+import TextField from '@mui/material/TextField';
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
+
 
 
 function Footer() {
+
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   
     return (
       <div className='foot-container'>
         <div>
-          <h3>Contact Me</h3>
+          <h2 className='contact'>Contact Me</h2>
           </div>
         <footer className='my-footer'>
           <a className='footer-icon' href='https://github.com/MoxUFO'><GitHubIcon sx={{color: 'rgb(34, 65, 92)'}} fontSize='large' /></a>
           <a className='footer-icon' href='https://www.linkedin.com/in/nelson-foster-8281a8279/'> <LinkedInIcon sx={{color: 'rgb(34, 65, 92)'}} fontSize='large'/></a>
-          <a className='footer-icon' href='https://twitter.com/MOX_UFO_'><TwitterIcon sx={{color: 'rgb(34, 65, 92)'}} fontSize='large'/></a>   
+          <a className='footer-icon' href='https://twitter.com/MOX_UFO_'><TwitterIcon sx={{color: 'rgb(34, 65, 92)'}} fontSize='large'/></a>
+
+        < AddIcCallIcon fontSize='large' onClick={handleClickOpen} sx={{color: 'rgb(34, 65, 92)'}}/>
+
+      <Dialog
+        open={open}
+        TransitionComponent={Transition}
+        keepMounted
+        onClose={handleClose}
+        aria-describedby="alert-dialog-slide-description"
+        sx={{color: 'rgb(34, 65, 92)'}}
+      >
+        <DialogTitle sx={{color: 'rgb(34, 65, 92)'}} >{"Thank you for stopping by!"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText sx={{color: 'rgb(34, 65, 92)'}} id="alert-dialog-slide-description">
+          <TextField
+          id="standard-textarea"
+          label="Name"
+          placeholder="Text"
+       
+          variant="standard"
+          fullWidth
+        />
+          <TextField
+          id="standard-textarea"
+          label="Email"
+          placeholder="Text"
+       
+          variant="standard"
+          fullWidth
+        />
+          <TextField
+          id="standard-textarea"
+          label="Message"
+          placeholder="Text"
+          multiline
+          variant="standard"
+          fullWidth
+        />
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+         
+          <Button sx={{color: 'rgb(34, 65, 92)'}} onClick={handleClose}>Submit</Button>
+        </DialogActions>
+      </Dialog>
         </footer>
       </div>
       
